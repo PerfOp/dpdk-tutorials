9243a91f73915ab3e7ed20ec95d4efe76252401f
huajianwang <huajianwang@microsoft.com>
2025-09-24 04:23:02 +0000

    MOD: Adding copy files

---
+++ bypass/rp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##############################################################
+# File Name: rs.sh
+# Author:
+# mail:
+# Created Time: Sat Sep 20 07:31:44 2025
+##############################################################
+sudo ./bin/bypass -c 0x1 -n 4 --proc-type=primary --file-prefix=bypass -- ring_buffer_1
+#sudo ../bin/bypass --lcores=0@0 -n 4 --proc-type=primary --file-prefix=bypass -- ring_buffer_1
