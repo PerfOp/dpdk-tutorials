485410587f37d49d45f9c98944b5443b5dcef07b
huajianwang <huajianwang@microsoft.com>
2025-09-25 01:09:08 +0000

    MOD: Adding new class for worker.

--- bypass/bpbuf_utils.h
+++ bypass/bpbuf_utils.h
@@ -6,8 +6,8 @@
 #include <rte_ethdev.h>
 #include <rte_mbuf.h>
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <string>
 #include <thread>
 
@@ -36,34 +36,41 @@ typedef struct sMemPool {
         pool_name = name;
         pool_handle = rte_pktmbuf_pool_create(
             pool_name.c_str(),  // Name of memory buffer pool.
-            count,               // Count of buffers in pool. (2048 - 1 = 2047)
-            RTE_MEMPOOL_CACHE_MAX_SIZE,  // Mempool cache size on cpu, 0: do not use cache.
-            0,  // Size of private area of memory buffer.
+            count,              // Count of buffers in pool. (2048 - 1 = 2047)
+            RTE_MEMPOOL_CACHE_MAX_SIZE,  // Mempool cache size on cpu, 0: do not
+                                         // use cache.
+            0,           // Size of private area of memory buffer.
             buffersize,  // Size of memory buffer.
-            port);  // Socket on which memory buffer is created.
+            port);       // Socket on which memory buffer is created.
 
         if (!pool_handle) {
-            printf_error("Unable to create a new pool %s. rte errno: %s\n",
+            spdlog::info("Unable to create a new pool {}. rte errno: {}",
                          pool_name.c_str(), rte_strerror(rte_errno));
             rte_eal_cleanup();
             exit(1);
+        } else {
+            spdlog::info("Pool {} : {} buffers {} bytes/per buffer.", pool_name,
+                         count, buffersize);
         }
+
         return true;
     }
 
     bool attach_pool(std::string name) {
         pool_name = name;
-        pool_handle = rte_mempool_lookup(pool_name.c_str());  // Name of memory buffer pool.
+        pool_handle = rte_mempool_lookup(
+            pool_name.c_str());  // Name of memory buffer pool.
 
         if (!pool_handle) {
-            printf_error("Unable to create a new pool %s. rte errno: %s\n",
+            spdlog::info("Unable to attach the pool {}. rte errno: {}",
                          pool_name.c_str(), rte_strerror(rte_errno));
             rte_eal_cleanup();
             exit(1);
+        } else {
+            spdlog::info("Pool {} attached.", pool_name);
         }
         return true;
     }
-
 
     rte_mbuf* const allocate_mbuf() {
         if (pool_handle == nullptr) {
@@ -71,7 +78,7 @@ typedef struct sMemPool {
         }
         rte_mbuf* const packet = rte_pktmbuf_alloc(pool_handle);
         if (!packet) {
-            bypass_log_error("Unable to allocate memory buffer. \n");
+            spdlog::error("Unable to alloc a buffer for packet!");
         }
         return packet;
     }
@@ -82,14 +89,13 @@ typedef struct sRingBuf {
     rte_ring* ring_handle{nullptr};
     bool create_ring(std::string name, uint32_t capacity, int port) {
         if (name.empty()) {
-            bypass_log_error("Please input valid ring name: %s\n",
-                             name.c_str());
+            spdlog::error("Please input valid ring name");
             return false;
         }
         if (ring_handle) {
-            bypass_log_error(
-                "Ring was Assigned a valid handle, do not reinitialize %s!\n",
-                name.c_str());
+            spdlog::error(
+                "Ring was Assigned a valid handle, do not reinitialize {}!",
+                name);
             return false;
         }
         ring_name = name;
@@ -101,37 +107,41 @@ typedef struct sRingBuf {
                                                // producer / Single consumer.
 
         if (!ring_handle) {
-            bypass_log_error("Unable to create ring buffer: %s RTE error: %s",
-                             ring_name.c_str(), rte_strerror(rte_errno));
+            spdlog::error("Unable to create ring: {} RTE error: {}", ring_name,
+                          rte_strerror(rte_errno));
             rte_eal_cleanup();
             exit(1);
+        } else {
+            spdlog::info("Ring created: {} with {} elements.", ring_name,
+                         capacity);
         }
         return true;
     }
-    bool attach_ring(std::string name){
+    bool attach_ring(std::string name) {
         ring_handle = rte_ring_lookup(name.c_str());
-        if (ring_handle == nullptr)
-        {
-            bypass_log_error("Unable to attach for ring buffer: %s RTE error:%s\n", name.c_str(), rte_strerror(rte_errno));
+        if (ring_handle == nullptr) {
+            spdlog::error("Unable to attach for ring buffer: {} RTE error:{}",
+                          name, rte_strerror(rte_errno));
             rte_eal_cleanup();
             exit(1);
-        }else{
-            ring_name=name;
-            log_info("bypass:","Attached ring buffer: %s\n", ring_name.c_str());
+        } else {
+            ring_name = name;
+            spdlog::info("Attached ring buffer: {}", ring_name);
         }
         return true;
     }
 
-    bool produce_packets(rte_mbuf* packet, uint16_t burst){
+    bool produce_packets(rte_mbuf* packet, uint16_t burst) {
         if (!rte_ring_enqueue(ring_handle, packet)) {
             return true;
-        }else{
+        } else {
             rte_pktmbuf_free(packet);
             return false;
         }
     }
-    uint32_t consume_packets(rte_mbuf** packets, uint32_t burst){
-        uint32_t rx_count = rte_ring_dequeue_burst(ring_handle, reinterpret_cast<void **>(packets), burst, nullptr);
+    uint32_t consume_packets(rte_mbuf** packets, uint32_t burst) {
+        uint32_t rx_count = rte_ring_dequeue_burst(
+            ring_handle, reinterpret_cast<void**>(packets), burst, nullptr);
         return rx_count;
     }
 } RingBuf;
---
+++ bypass/nic_worker.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) The software Authors. All rights reserved.
+ * File Name: bypass/nic_worker.h
+ * Author:
+ * mail:
+ * Created Time: Wed Sep 24 05:20:07 2025
+ * Brief:
+ */
+
+#ifndef NIC_WORKER_H
+#define NIC_WORKER_H
+
+#include <csignal>
+#include "bpbuf_utils.h"
+#include "debug_utils.h"
+#include "time_helper.h"
+
+class NicProcess {
+public:
+    NicProcess() {}
+    virtual ~NicProcess() {}
+    bool InitNicResource();
+    int MainLoop() { return recv_loop(); }
+
+private:
+    bool initnicport();
+    int recv_loop();
+    void issue_request();
+    uint64_t m_total_rx_packets = 0;
+    // LiteQueue *m_attachDataQueue;
+    DynaQueue m_attachDataQueue;
+
+    Stats nicStats;
+    MemPool m_dataPool;
+    RingBuf m_dataRing;
+
+    MemPool m_cmdPool;
+    RingBuf m_cmdRing;
+};
+
+#endif // NIC_WORKER_H
