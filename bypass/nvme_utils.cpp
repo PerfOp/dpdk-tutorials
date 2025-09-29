7ed686f1830029f832bbf19989cdcb12aa79ec90
huajianwang <huajianwang@microsoft.com>
2025-09-29 08:39:57 +0000

    MOD: Adding Makefile

---
+++ bypass/nvme_utils.cpp
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) The software Authors. All rights reserved.
+ * File Name: bypass/nvme_utils.cpp
+ * Author:
+ * mail:
+ * Created Time: Wed Sep 24 05:20:07 2025
+ * Brief:
+ */
+#include "nvme_utils.h"
+
+// #include "spdk/stdinc.h"
+// #include "spdk/nvme.h"
+// // #include "spdk_internal/nvme_util.h"
+// #include "spdk/vmd.h"
+// #include "spdk/nvme_zns.h"
+// #include "spdk/env.h"
+// #include "spdk/string.h"
+// #include "spdk/log.h"
+
+bool probe_nvme(void){
+    // struct spdk_env_opts opts;
+    // // spdk_env_opts_init(&opts);
+    // opts.name="nvme_pcid_rw";
+    // spdk_env_init(&opts);
+    return true;
+}
---
+++ bypass/nvme_utils.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) The software Authors. All rights reserved.
+ * File Name: bypass/nvme_utils.h
+ * Author:
+ * mail:
+ * Created Time: Wed Sep 24 05:20:07 2025
+ * Brief:
+ */
+
+#ifndef NVME_UTILS_H
+#define NVME_UTILS_H
+
+bool probe_nvme(void);
+
+#endif // NVME_UTILS_H
